33dfe573698f7ec702635b79b79181216c1fbca0
Sukeesh <vsukeeshbabu@gmail.com>
2016-07-08 13:57:16 +0530

    Rewritten complete code using Youtube Data API

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-#Music-Downloader
-A Simple Hack to download any music from web
+#  Music Downloader :notes: :musical_score: :trumpet: :violin:
+
+> A Simple hack to download any music from web
+
+Enter Songname / Lyrics or whatever..<br>
+Search, Download and Enjoy!
 
 See it in action here : http://104.236.135.241/music/
 
-Enter Songname / Lyrics or whatever.. <br>
-Search ,Download and Enjoy!
+If you want to test it locally, 
+get your Youtube data API key from https://console.cloud.google.com/apis/api/youtube/overview
+
+## Disclaimer
+Downloading copyrighted material may be illegal in your country. Use at your own risk.
+
+
+© Sukeesh
---
+++ js/app.js
@@ -0,0 +1,34 @@
+function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+
+$(function() {
+    $("form").on("submit", function(e) {
+       e.preventDefault();
+
+       var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 5,
+            order: "viewCount"
+       });
+
+       request.execute(function(response) {
+          var results = response.result;
+          $("#results").html("");
+          $.each(results.items, function(index, item) {
+            $.get("tpl/item.html", function(data) {
+                $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+            });
+          });
+          resetVideoHeight();
+       });
+    });
+    
+    $(window).on("resize", resetVideoHeight);
+});
+
+function init() {
+    gapi.client.setApiKey("__YOUR_API_KEY__");   // Your API Key here
+    gapi.client.load("youtube", "v3", function() {
+    });
+}
