7863f1a1470982da1c8f8028e9f3f06d50a00f1c
Sukeesh <sukeesh@Sukeeshs-MacBook-Air.local>
2017-06-20 14:19:08 +0530

    MaterialDesign

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{
 $(function() {
     $("form").on("submit", function(e) {
        e.preventDefault();
-        $("#results").html('<img src="js/loader.gif" alt="loading" />');
+	$("#results").html('<div class="preloader-wrapper small active"> <div class="spinner-layer spinner-blue"> <div class="circle-clipper left"> <div class="circle"></div></div><div class="gap-patch"> <div class="circle"></div></div><div class="circle-clipper right"> <div class="circle"></div></div></div><div class="spinner-layer spinner-red"> <div class="circle-clipper left"> <div class="circle"></div></div><div class="gap-patch"> <div class="circle"></div></div><div class="circle-clipper right"> <div class="circle"></div></div></div><div class="spinner-layer spinner-yellow"> <div class="circle-clipper left"> <div class="circle"></div></div><div class="gap-patch"> <div class="circle"></div></div><div class="circle-clipper right"> <div class="circle"></div></div></div><div class="spinner-layer spinner-green"> <div class="circle-clipper left"> <div class="circle"></div></div><div class="gap-patch"> <div class="circle"></div></div><div class="circle-clipper right"> <div class="circle"></div></div></div></div>');
        var request = gapi.client.youtube.search.list({
             part: "snippet",
             type: "video",
             q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-            maxResults: 5,
-            order: "viewCount"
+            maxResults: 6,
+            order: "relevance"
        });
        request.execute(function(response) {
           var results = response.result;
@@ -19,13 +19,13 @@ $(function() {
                 $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
             });
           });
-          resetVideoHeight();
        });
     });
 });
 
 function init() {
-    gapi.client.setApiKey("__YOUR_API_KEY__");   // Your API Key here
+    gapi.client.setApiKey("AIzaSyDJzmBQREJ-RGJFY0cPvvVGLv02FZMQbfc");
     gapi.client.load("youtube", "v3", function() {
     });
 }
+
